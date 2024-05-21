a22d88893c6b0b8e08dcaa76bab635b62ccae194
Thielke <jonatas1927@gmail.com>
2024-05-21 01:55:15 -0300

    fix set provbiders on backedn

--- backend/src/integrations/integrations.module.ts
+++ backend/src/integrations/integrations.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { IntegrationsService } from './integrations.service';
 import { IntegrationsController } from './integrations.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  providers: [IntegrationsService],
+  providers: [IntegrationsService, PrismaService],
   controllers: [IntegrationsController]
 })
-export class IntegrationsModule {}
+export class IntegrationsModule { }
